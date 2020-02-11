7d96737e033f494085b8149efbce0ff137d1d282
iris5115 <yukurio5115iris@gmail.com>
2020-02-11 14:05:02 +0900

    create_autimatic_updating
    ChatSpace自動更新機能の追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,44 +1,79 @@
 $(function(){ 
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-      `<div class="message" data-message-id=${message.id}>
-        <ul class="message__lists">
-          <li class="message__lists__user-name">
-            ${message.user_name}
-          </li>
-          <li class="message__lists__daytime">
-            ${message.created_at}
-          </li>
-        </ul>
-        <div class="message__content">
-          <p class="message__content__text">
-            ${message.content}
-          </p>
-        </div>
-        <img src=${message.image} >
-      </div>`
-      return html;
-    } else {
-      var html =
-      `<div class="message" data-message-id=${message.id}>
-        <ul class="message__lists">
-          <li class="message__lists__user-name">
-            ${message.user_name}
-          </li>
-          <li class="message__lists__daytime">
-            ${message.created_at}
-          </li>
-        </ul>
-        <div class="message__content">
-          <p class="message__content__text">
-            ${message.content}
-          </p>
-        </div>          
-      </div>`
-      return html;
+  last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id);
+  var buildHTML = function(message){
+    if (message.content && message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__lists">` +
+          `<div class="message__lists__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__lists__daytime">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__content">` +
+          `<p class="message__content__text">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="message__content__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__lists">` +
+          `<div class="message__lists__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__lists__daytime">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__content">` +
+          `<p class="message__content__text">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="message" data-message-id=` + message.id + `>` +
+        `<div class="message__lists">` +
+          `<div class="message__lists__user-name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="message__lists__daytime">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="message__content">` +
+          `<img src="` + message.image + `" class="message__content__image" >` +
+        `</div>` +
+      `</div>`
     };
-  }
+    return html;
+  };
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
 $('#new_message').on('submit', function(e){
   e.preventDefault();
   var formData = new FormData(this);
@@ -62,4 +97,7 @@ $('#new_message').on('submit', function(e){
     alert("メッセージ送信に失敗しました");
 });
 })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
